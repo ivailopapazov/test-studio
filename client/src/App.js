4ff05f6c1b92f43522a20cddff0e4fcb79b20666
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2020-04-16 17:59:19 +0300

    Base initial project

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState({}); 
+
+  useEffect(() => {
+    fetch('http://localhost:5000/people/10')
+      .then(res => res.json())
+      .then(data => setData(data))
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{data.name}</h1>
+        <h2>{data.id}</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
